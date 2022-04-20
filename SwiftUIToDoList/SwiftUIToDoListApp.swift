54d745bdfc48ee01d839a05badbeb96740f0d395
Lucas Barbosa <lucasbarbosa2807@gmail.com>
2022-04-20 10:07:18 -0300

    Initial Commit

---
+++ SwiftUIToDoList/SwiftUIToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToDoListApp.swift
+//  SwiftUIToDoList
+//
+//  Created by Lucas Barbosa de Oliveira on 20/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
